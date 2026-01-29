229e67564e6bc33de1296ed2b60d9c6994de32c1
pnppl <pnppl>
2026-01-29 04:49:12 -0500

    deploy.fish 2026-01-29 04:49:12-05:00

--- deploy.fish
+++ deploy.fish
@@ -1,1 +1,3 @@
-git add . && git commit -m "deploy.fish (date --rfc-3339='seconds')" && git push
+fish build.fish &&
+git add . && git commit -m "deploy.fish $(date --rfc-3339='seconds')" && git push &&
+echo "! DEPLOY OK !"
