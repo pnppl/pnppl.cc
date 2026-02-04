a9377d96c462bbc8647bd84f595ebca2bbd59457
pnppl <pnppl>
2026-02-04 03:26:55 -0500

    sigh

--- deploy.fish
+++ deploy.fish
@@ -3,11 +3,11 @@ set msg "deploy.fish $(date --rfc-3339='seconds')" &&
 if set -q _flag_commit
 	set msg $_flag_commit
 end
-git pull &&
 rm -rf site/public/ &&
 fish build.fish &&
-chmod -R 755 site/ &&
-git add . && git commit -m "$msg" && git push &&
+git add . && git commit -m "$msg" &&
+git pull &&
+git push &&
 lftp -e "set ftp:skey-force; mirror -R site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
 #lftp -e "set ftp:skey-force; set ftp:ssl-protect-data yes; set ftp:ssl-protect-list yes; mirror -R site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
 echo "! DEPLOY OK !"
