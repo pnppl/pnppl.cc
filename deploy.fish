abdba37ef8ed37ebfe82dc052deac626d2edf5d0
pnppl <pnppl>
2026-02-04 07:13:23 -0500

    more mobile fun

--- deploy.fish
+++ deploy.fish
@@ -1,15 +1,23 @@
-argparse 'c/commit=?' -- $argv &&
+argparse 'c/commit=?' 'm/mobile=?' -- $argv &&
 set msg "deploy.fish $(date --rfc-3339='seconds')" &&
 if set -q _flag_commit
 	set msg $_flag_commit
+end
+if set -q _flag_mobile
+	rsync -avh --progress --update "$_flag_mobile" site/txt/
 end
 rm -rf site/public/ &&
 fish build.fish &&
 for file in (path filter -t dir (fdfind . site/)); chmod 755 $file; end
 for file in (path filter -t file (fdfind . site/)); chmod 644 $file; end
-metastore -s -m -f meta.store &&
-git add . && git commit -m "$msg" &&
+git add . &&
+git stash &&
 git pull &&
+metastore -a -m -f meta.store &&
+git stash pop &&
+metastore -s -m -f meta.store &&
+git add . &&
+git commit -m "$msg" &&
 git push &&
 lftp -e "set ftp:skey-force; mirror -R site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
 #lftp -e "set ftp:skey-force; set ftp:ssl-protect-data yes; set ftp:ssl-protect-list yes; mirror -R site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
