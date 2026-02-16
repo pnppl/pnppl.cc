2fe9df5e3005727ae17515a853e4fd7838d00518
pnppl <pnppl>
2026-02-16 13:09:04 -0500

    ensure code renders in monospace font; hopefully upload faster over ftp

--- deploy.fish
+++ deploy.fish
@@ -32,7 +32,7 @@ zip -r site/img/1bitday/!1bitday.zip site/img/1bitday/ &&
 chmod -R 755 site/ &&
 #for file in (path filter -t dir (fdfind . site/)); chmod 755 $file; end
 #for file in (path filter -t file (fdfind . site/)); chmod 655 $file; end
-lftp -e "set ftp:skey-force; mirror -R --parallel 10 --delete site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
+lftp -e "set ftp:skey-force; mirror -R --parallel=10 --delete site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
 for file in (path filter -t file (fdfind . site/)); chmod -x $file; end &&
 echo "! DEPLOY OK !" ||
 echo " !! ~~~~~~~ DEPLOY FAILED! ~~~~~~ !! "
