656dc122172dcd224e46be44ba6379ab0c66817e
pnppl <pnppl>
2026-02-22 16:17:25 -0500

    nothin

--- deploy.fish
+++ deploy.fish
@@ -33,7 +33,7 @@ chmod -R 755 site/ &&
 #for file in (path filter -t dir (fdfind . site/)); chmod 755 $file; end
 #for file in (path filter -t file (fdfind . site/)); chmod 655 $file; end
 lftp -e "set ftp:skey-force; mirror -R --parallel=20 --delete site/ /; exit" -u pnppl,$FTP_PASSWORD w10.host &&
-for file in (path filter -t file (fdfind . site/)); chmod -x $file; end &&
+for file in (fdfind -I -t f . site/); chmod -x $file; end &&
 echo "! DEPLOY OK !" ||
 echo " !! ~~~~~~~ DEPLOY FAILED! ~~~~~~ !! "
 
