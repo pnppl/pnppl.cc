d0acb32f829135ca40ab96ba91d8a49901044f92
pnppl <pnppl>
2026-02-23 00:05:47 -0500

    deploy.fish 2026-02-23 00:05:36-05:00

--- build.fish
+++ build.fish
@@ -2,6 +2,7 @@ rm -rf site/public/ &&
 rm -rf site/+/ &&
 #fish prep_title.fish &&
 set pwd (pwd) &&
+minify xlog/public/style.css -o xlog/public/style-min.css &&
 cd xlog/cmd/xlog &&
 go run xlog.go -source $pwd/site/txt -build $pwd/site -sitename pnppl.cc -rss.domain pnppl.cc -sitemap.domain pnppl.cc -og.domain pnppl.cc &&
 #cd $pwd &&
