453284c9e2eec0e6542b4112fbaec1ae649c07b5
pnppl <pnppl>
2026-02-23 23:14:36 -0500

    csssssssssssss

--- build.fish
+++ build.fish
@@ -3,6 +3,7 @@ rm -rf site/+/ &&
 #fish prep_title.fish &&
 set pwd (pwd) &&
 minify xlog/public/style.css -o xlog/public/style-min.css &&
+sed -i -z 's/[\n\t]//g' xlog/public/style-min.css &&
 cd xlog/cmd/xlog &&
 go run xlog.go -source $pwd/site/txt -build $pwd/site -sitename pnppl.cc -rss.domain pnppl.cc -sitemap.domain pnppl.cc -og.domain pnppl.cc &&
 #cd $pwd &&
