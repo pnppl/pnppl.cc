aa31efab1f4f98f6960ccd6a25147ee951aa2705
pnppl <pnppl>
2026-04-02 08:22:03 -0400

    fix epub again

--- epub/epub.fish
+++ epub/epub.fish
@@ -1,4 +1,5 @@
 set scratch_dir './.epub_tmp'
+set out 'site/txt/!pnppl.epub'
 #set excluded 404 index app 1-bit.day fs-ir visual-art
 rm -rf $scratch_dir
 mkdir $scratch_dir &&
@@ -24,18 +25,21 @@ for file in site/txt/20*.md
 		# fix footnotes without fucking everything up with pandoc's inscrutable --file-scope
 #		sed -i -E "s/\[\^([[:digit:]]+)\]/\[\^$(path basename -E $file)-\1\]/g" $filename
 		sed -i -E "s/\[\^/\[\^$(path basename -E $file)-/g" $filename
-		# remove video embeds. open and close need to be on same line
-		sed -i -E 's/<video.+<\/video>//g' $filename
+		# remove A/V embeds. open and close need to be on same line
+		sed -i -E 's/!\[\[.+(mp4|mkv|webm|mp3|wav|flac|ogg|ogv|oga|m4a)\]\]$//g' $filename
 #	end
 end
 set markdown_features markdown-smart+ascii_identifiers+space_in_atx_header+backtick_code_blocks+fenced_code_attributes+pipe_tables+strikeout+footnotes+lists_without_preceding_blankline+hard_line_breaks+autolink_bare_uris+wikilinks_title_before_pipe+header_attributes
-pandoc -o site/txt/!pnppl.epub -f $markdown_features --toc --reference-location=section --metadata title="pnppl.cc" --epub-metadata="epub/epub.xml" --epub-title-page=false --css="epub/epub.css" --lua-filter="epub/chapters.lua" --resource-path=site/txt $scratch_dir/*.md &&
+pandoc -o $out -f $markdown_features --toc --reference-location=section --metadata title="pnppl.cc" --epub-metadata="epub/epub.xml" --epub-title-page=false --css="epub/epub.css" --lua-filter="epub/chapters.lua" --resource-path=site/txt $scratch_dir/*.md &&
 if test $status -ne 0
 	echo "!!----- EPUB FUCKED UP -----!!"
 	set return 1
 else
 	echo "! epub success !"
 	set return 0
+end
+if test (du $out | cut -f1) -ge 1024
+	echo "!!~~ WARNING: epub larger than 1MB ~~!!"
 end
 rm -rf $scratch_dir
 return $return
