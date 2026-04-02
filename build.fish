80fa5e823cebce616ad1d2bd8b02d4699664268c
pnppl <pnppl>
2026-04-02 09:13:29 -0400

    undo stylesheet fragmenting

--- build.fish
+++ build.fish
@@ -1,10 +1,8 @@
 rm -rf site/public/ &&
 rm -rf site/+/ &&
 set pwd (pwd) &&
-minify xlog/public/style.css -o xlog/public/style-min.css &&
-minify xlog/public/print.css -o xlog/public/print-min.css &&
-sed -i -z 's/[\n\t]//g' xlog/public/style-min.css &&
-sed -i -z 's/[\n\t]//g' xlog/public/print-min.css &&
+minify xlog/public/style.css |
+sed -z 's/[\n\t]//g' >> xlog/public/style-min.css &&
 cd xlog/cmd/xlog &&
 go run xlog.go -source $pwd/site/txt -build $pwd/site -sitename pnppl.cc -rss.domain pnppl.cc -sitemap.domain pnppl.cc -og.domain pnppl.cc &&
 cd $pwd &&
