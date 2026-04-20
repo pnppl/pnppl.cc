2e17a7daff9716850b582687459be7c9f1d26665
pnppl <pnppl>
2026-04-20 15:52:03 -0400

    render autolink tooltips and decoration

--- xlog/extensions/autolink_pages/renderer.go
+++ xlog/extensions/autolink_pages/renderer.go
@@ -2,7 +2,9 @@ package autolink_pages
 
 import (
 	"fmt"
-
+	"slices"
+	"strings"
+	"github.com/emad-elsaid/xlog"
 	"github.com/emad-elsaid/xlog/markdown/ast"
 	"github.com/emad-elsaid/xlog/markdown/renderer"
 	"github.com/emad-elsaid/xlog/markdown/util"
@@ -19,9 +21,34 @@ func render(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.
 		n := node.(*PageLink)
 		url := n.page.Name()
 
+		// identify linked document by its hashtags and put definitions in tooltip
+		class := "autolink"
+		tooltip := ""
+		parent := xlog.NewPage(url)
+		props := xlog.Properties(parent)
+		if v, ok := props["tags"]; ok && v != nil {
+			if slices.Contains(v.Value().([]string), "def") {
+				class = class + " def"
+				lines := strings.Split(string(parent.Content()), "\n")
+				for _, line := range lines {
+					if len(line) > 0 {
+						if line[0] != byte('#') {
+							tooltip = tooltip + string(line) + " "
+						}
+					}
+				}
+//				tooltip = []byte(parent.Content())
+			}
+		}
+		if len(tooltip) == 0 {
+			tooltip = "mention"
+		}
 		fmt.Fprintf(w,
-			`<a class="autolink" href="/%s">`,
+			`<a href="/%s" class="%s" title="%s">`,
 			util.EscapeHTML(util.URLEscape([]byte([]byte(url)), false)),
+			class,
+			util.EscapeHTML([]byte(strings.Trim(tooltip, " \t"))),
+//			tooltip,
 		)
 
 		if total, done := countTodos(n.page); total > 0 {
