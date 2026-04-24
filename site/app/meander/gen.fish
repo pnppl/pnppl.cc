e2d162a8eb653899b41aabf4939e2530faf4551d
pnppl <pnppl>
2026-04-24 03:40:44 -0400

    deploy.fish 2026-04-24 03:40:21-04:00

--- site/app/meander/gen.fish
+++ site/app/meander/gen.fish
@@ -1,3 +1,8 @@
 cat links.md | grep '^h' > .links.txt &&
 sed -i 's|http://|https://|g' .links.txt &&
 fish ../nagi/generate.fish --in=".links.txt" --out=".p"
+
+export LC_ALL=C &&
+for line in (sort blocked.txt)
+	echo "<a href=\"$line\">$line</a><br>" >> blocked.html
+end
